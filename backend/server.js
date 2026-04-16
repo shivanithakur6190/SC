52a338835f1d19e42ed22b25f82fa066908f5304
Shivani Thakur <shivnithkur6190@gmail.com>
2026-04-16 17:19:47 +0530

    Added env support

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect("mongodb://127.0.0.1:27017/trello")
+require("dotenv").config();
+mongoose.connect("process.env.MONGO_URI")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
